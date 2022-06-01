31ea17c802bf8a61d9aacf4928076b902d5b4aaa
o2sevruk <o2sevruk@gmail.com>
2022-06-01 12:42:15 +0300

    feat: implement fs list.

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,17 @@
+import {access, constants} from 'fs';
+import {readdir} from 'fs/promises';
+
 export const list = async () => {
-    // Write your code here 
+  const dir = 'files';
+
+  await access(dir, constants.F_OK, async (isExist) => {
+    if (isExist) {
+      throw new Error('FS operation failed');
+    }
+
+    const files = await readdir(dir);
+    console.log(files);
+  });
 };
+
+list();
