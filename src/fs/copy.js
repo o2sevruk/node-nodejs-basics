a31b548344b2d7fcb570313eabcc8b9af57a0fb6
o2sevruk <o2sevruk@gmail.com>
2022-06-01 12:28:20 +0300

    feat: implement fs copy.

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,31 @@
+import {access, constants} from 'fs';
+import {mkdir, readdir, copyFile} from 'fs/promises';
+
 export const copy = async () => {
-    // Write your code here 
+  const baseDir = 'files';
+  const targetDir = 'files_copy';
+
+  await access(targetDir, constants.F_OK, async (isExist) => {
+    if (!isExist) {
+      throw new Error('FS operation failed');
+    }
+
+    try {
+      const files = await readdir(baseDir);
+
+      // Create folder if not exist
+      await mkdir(targetDir);
+      console.log('Folder was created!');
+
+      // Copy files
+      for (const file of files) {
+        await copyFile(`${baseDir}/${file}`, `${targetDir}/${file}`);
+      }
+      console.log('Files were copied!');
+    } catch (err) {
+      throw new Error(err);
+    }
+  });
 };
+
+copy();
