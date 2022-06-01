e6cf098bd6086a0ef2034dc2b7d08fa706a502e3
o2sevruk <o2sevruk@gmail.com>
2022-06-01 12:49:46 +0300

    feat: implement fs read.

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,17 @@
+import {access, constants} from 'fs';
+import {readFile} from 'fs/promises';
+
 export const read = async () => {
-    // Write your code here 
+  const file = 'files/fileToRead.txt';
+
+  await access(file, constants.F_OK, async (isNotExist) => {
+    if (isNotExist) {
+      throw new Error('FS operation failed');
+    }
+
+    const fileText = await readFile(file, 'utf8');
+    console.log(fileText);
+  });
 };
+
+read();
