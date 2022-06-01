af8e38d320b9d3dab916f7c106de408661cf748d
o2sevruk <o2sevruk@gmail.com>
2022-06-01 12:28:28 +0300

    feat: implement fs create.

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,17 @@
+import {access, constants} from 'fs';
+import {writeFile} from 'fs/promises';
+
 export const create = async () => {
-    // Write your code here 
+  const newFile = 'files/fresh.txt';
+
+  await access(newFile, constants.F_OK, (isExist) => {
+    if (!isExist) {
+      throw new Error('FS operation failed');
+    }
+
+    writeFile(newFile, 'I am fresh and young');
+    console.log('File was created!');
+  });
 };
+
+create();
