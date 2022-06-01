5cfefd7ce454d465d8fffdb4f8010a43dd76a014
o2sevruk <o2sevruk@gmail.com>
2022-06-01 12:35:57 +0300

    feat: implement fs delete.

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,17 @@
+import {access, constants} from 'fs';
+import {rm} from 'fs/promises';
+
 export const remove = async () => {
-    // Write your code here 
+  const file = 'files/fileToRemove.txt';
+
+  await access(file, constants.F_OK, async (isExist) => {
+    if (isExist) {
+      throw new Error('FS operation failed');
+    }
+
+    await rm(file);
+    console.log('File was deleted!');
+  });
 };
+
+remove();
