efa79ea8663117e85c1526be83f74e0216ed4d0d
o2sevruk <o2sevruk@gmail.com>
2022-06-02 12:17:34 +0300

    feat: implement hash calcHash.

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,20 @@
+import {createHash} from 'crypto';
+import {access, constants} from 'fs';
+import {readFile} from 'fs/promises';
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const file = 'files/fileToCalculateHashFor.txt';
+
+  await access(file, constants.F_OK, async (err) => {
+    if (err) {
+      throw new Error('FS operation failed');
+    }
+
+    const textFromFile = await readFile(file, 'utf8');
+    const hash = createHash('sha256').update(textFromFile).digest('hex');
+
+    console.log(hash);
+  });
 };
+
+calculateHash();
