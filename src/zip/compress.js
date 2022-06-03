532642eb8242aa8738aed8f6aeb0fbad831a5412
o2sevruk <o2sevruk@gmail.com>
2022-06-03 21:39:47 +0300

    feat: implement zlib compress.

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,20 @@
+import {createReadStream, createWriteStream} from 'fs';
+import {createGzip} from 'zlib';
+import {pipeline} from 'stream';
+
 export const compress = async () => {
-    // Write your code here 
+  const fileSource = 'files/fileToCompress.txt';
+  const fileDestination = 'files/archive.gz';
+
+  const gzip = createGzip();
+  const source = createReadStream(fileSource);
+  const destination = createWriteStream(fileDestination);
+
+  pipeline(source, gzip, destination, (err) => {
+    if (err) {
+      throw new Error('Zlib operation failed');
+    }
+  });
 };
+
+compress();
