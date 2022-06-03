35ff456c25c09acab574bc91313be305b24390f1
o2sevruk <o2sevruk@gmail.com>
2022-06-03 21:27:59 +0300

    feat: implement streams transform.

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,22 @@
+import {createInterface} from 'readline';
+import {Transform} from 'stream';
+
 export const transform = async () => {
-    // Write your code here 
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+  });
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, `${chunk.toString().split('').reverse().join('')}\n`);
+    },
+  });
+
+  transformStream.pipe(process.stdout);
+  rl.on('line', (data) => {
+    transformStream.write(data);
+  });
 };
+
+transform();
