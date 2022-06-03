b861e9b25192bb3f7514fa24425933b2040de52d
o2sevruk <o2sevruk@gmail.com>
2022-06-03 21:47:13 +0300

    feat: implement zlib decompress.

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,20 @@
+import {createReadStream, createWriteStream} from 'fs';
+import {createGunzip} from 'zlib';
+import {pipeline} from 'stream';
+
 export const decompress = async () => {
-    // Write your code here 
+  const fileSource = 'files/archive.gz';
+  const fileDestination = 'files/fileToCompress.txt';
+
+  const gunzip = createGunzip();
+  const source = createReadStream(fileSource);
+  const destination = createWriteStream(fileDestination);
+
+  pipeline(source, gunzip, destination, (err) => {
+    if (err) {
+      throw new Error('Zlib operation failed');
+    }
+  });
 };
+
+decompress();
