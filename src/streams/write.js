5deeda84c4e760b5da89507405b4e7b4dc4ad27a
o2sevruk <o2sevruk@gmail.com>
2022-06-03 20:52:54 +0300

    feat: implement streams write.

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,18 @@
+import {createInterface} from 'readline';
+import {createWriteStream} from 'fs';
+
 export const write = async () => {
-    // Write your code here 
+  const file = 'files/fileToWrite.txt';
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+  });
+  const writableStream = createWriteStream(file);
+
+  rl.on('line', (data) => {
+    writableStream.write(`${data}\n`);
+  });
 };
+
+write();
