f22760fd0fb62565218b5571663defe3a971f522
o2sevruk <o2sevruk@gmail.com>
2022-06-05 17:04:53 +0300

    feat: implement wt main.

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,34 @@
+import {cpus} from 'os';
+import {Worker} from 'worker_threads';
+
 export const performCalculations = async () => {
-    // Write your code here
+  const cpuCores = cpus().length;
+  const promises = [];
+
+  for (let i = 0; i < cpuCores; i++) {
+    const newPromise = async () => {
+      return new Promise((res) => {
+        const worker = new Worker('./worker.js', {workerData: 10 + i});
+        worker.on('message', (msg) => {
+          res({
+            status: 'resolved',
+            data: msg,
+          });
+        });
+        worker.on('error', () => {
+          res({
+            status: 'error',
+            data: null,
+          });
+        });
+      }).then(data => console.log(data));
+    };
+    promises.push(newPromise);
+  }
+
+  for(const promise of promises) {
+    await promise();
+  }
 };
+
+performCalculations();
