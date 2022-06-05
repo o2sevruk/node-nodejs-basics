01c1932d7b084676215fbe34d13caaed4a7dc934
o2sevruk <o2sevruk@gmail.com>
2022-06-05 18:15:05 +0300

    feat: implement cp cp.

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,16 @@
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+import {spawn} from 'child_process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = spawn('node', [`${__dirname}/files/script.js`, ...args]);
+
+  child.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
 };
+
+spawnChildProcess(['test1', 'test2']);
